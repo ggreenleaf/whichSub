e0ccb2f1c13bb94765258654326e5db7f3d52c31
ggreenleaf <geoffrey.greenleaf@gmail.com>
2016-07-31 15:53:23 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name whichSubApp
+ * @description
+ * # whichSubApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('whichSubApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name whichSubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the whichSubApp
+ */
+angular.module('whichSubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
